58f0c3a77f8f5081a33379a03bd17646ecf42323
Steven Barnhurst <stevenbarnhurst@gmail.com>
2015-11-02 11:46:15 +0100

    init

--- config/env/default.js
+++ config/env/default.js
@@ -2,9 +2,9 @@
 
 module.exports = {
   app: {
-    title: 'MEAN.JS',
-    description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
-    keywords: 'mongodb, express, angularjs, node.js, mongoose, passport',
+    title: 'deutsch',
+    description: 'Education application for learning the basics of German.',
+    keywords: 'MongoDB, Express, AngularJS, Node.js',
     googleAnalyticsTrackingID: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'GOOGLE_ANALYTICS_TRACKING_ID'
   },
   port: process.env.PORT || 3000,
